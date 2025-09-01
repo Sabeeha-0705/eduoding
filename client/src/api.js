c351ee2926380f491fd6ccb3672294aaaa802f51
Sabeeha-0705 <sabeehafathimap@gmail.com>
2025-09-01 22:25:18 +0530

    Updated API base URL to Render backend

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://eduoding-backend.onrender.com",
 });
 
 // 🔐 Interceptor to attach token
