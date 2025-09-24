68358af19924aecc9eb5c3cbda5ea1fc34a62f9b
Sabeeha-0705 <sabeehafathimap@gmail.com>
2025-09-24 20:46:51 +0530

    Fix the Error

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,7 +8,7 @@ import {
   forgotPassword,
   resetPassword,
 } from "../controllers/authController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import  protect  from "../middleware/authMiddleware.js";
 
 const router = Router();
 
