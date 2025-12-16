2fd891a376689e09615cdd19db028ca25a726101
Sabeeha-0705 <sabeehafathimap@gmail.com>
2025-12-16 20:40:43 +0530

    ok

--- eduoding-mobile/context/AuthContext.jsx
+++ eduoding-mobile/context/AuthContext.jsx
@@ -1,6 +1,9 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import API from "../services/api";
+import API from "../app/services/api";
+
+
+
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
